c659cea20e277766e62b3c9e6bee5b6c2ed26edf
Lyze <11274700+lyze237@users.noreply.github.com>
2023-09-19 21:24:17 +0200

    Fixed inconsistencies

--- scripts/player/components/PlayerRotateToDirection.cs
+++ scripts/player/components/PlayerRotateToDirection.cs
@@ -9,44 +9,36 @@ public partial class PlayerRotateToDirection : Node
 
 	private Vector3 lookDir = Vector3.Zero;
 	private Vector2 lastNonNullDirection = Vector2.Up;
-	private RandomNumberGenerator rng = new RandomNumberGenerator();
 
 	public override void _PhysicsProcess(double delta)
 	{
 		var lookAtRotationRad = player.Pivot.Rotation.Y;
 
-		var targetLookAt = calculateTargetLookAt();
+		var targetLookAt = CalculateTargetLookAt();
 		var targetLookAtRad = targetLookAt.SignedAngleTo(Vector3.Forward, Vector3.Down);
 
 		var rotatedAngle = Mathf.LerpAngle(lookAtRotationRad, targetLookAtRad, delta * speed);
-		float deltaAngle = (float)(rotatedAngle - lookAtRotationRad);
+		var deltaAngle = (float)(rotatedAngle - lookAtRotationRad);
+		
 		player.Pivot.Rotate(Vector3.Up, deltaAngle);
 	}
 
-	public Vector3 calculateTargetLookAt()
+	private Vector3 CalculateTargetLookAt()
 	{
-		lazyUpdateLastInputDir();
+		LazyUpdateLastInputDir();
+		
 		var dir = lastNonNullDirection;
-
 		return (player.Transform.Basis * new Vector3(dir.X, 0, dir.Y)).Normalized();
 	}
 
-	public void lazyUpdateLastInputDir()
+	private void LazyUpdateLastInputDir()
 	{
 		var dir = player.Input.Direction;
-		if (hasDirectionInput(dir))
-		{
+		
+		if (HasDirectionInput(dir)) 
 			lastNonNullDirection = dir;
-		}
 	}
 
-	private bool hasDirectionInput(Vector2 dir)
-	{
-		if (dir is { X: 0, Y: 0 })
-		{
-			return false;
-		}
-
-		return true;
-	}
+	private static bool HasDirectionInput(Vector2 dir) => 
+		dir is not { X: 0, Y: 0 };
 }
